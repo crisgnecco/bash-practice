8187f16de3c2facee0e2208322766cd7f2e37e70
Cristian <gneccocristin@gmail.com>
2020-09-07 15:51:34 -0300

    agregado: ejemplos de clase

--- eje2/copia.sh
+++ eje2/copia.sh
@@ -1,4 +1,4 @@
-fileName=$1
+ruta=$1
 while true ;do
 
   #Test:If 6 is greater than 5, output yes, otherwise output no.
@@ -7,11 +7,15 @@ while true ;do
   hora=`date +%H%M%S`
   
   #if [ -d $fileName ] 
-  if test -d $path 
+  if test -d $ruta                      # que verifica esto????
   then
-    newPath=${fileName##*/}
+    newPath=${ruta##*/}
+    echo newPath
     newFileName="$newPath$fecha$hora"
     cp -r $newPath $newFileName
+  else                                  # para que sirve esta parte???
+    newFileName="$ruta$fecha$hora"
+    cp -r $ruta $newFileName
   fi
   sleep $2
 done
